9bfd9dcf72f0b366b401e656da4585ff604b6909
Maxime Tassaert <m.tassaert@outlook.fr>
2023-12-12 15:07:50 +0100

    aaa

--- Cube C - API/Models/Address.cs
+++
@@ -1,8 +0,0 @@
-namespace Cube_C___API.Models;
-
-public class Address
-{
-    public int Id { get; set; }
-    
-    public string name { get; set; }
-}
--- Cube C - API/Models/AppContext.cs
+++
@@ -1,9 +0,0 @@
-namespace Cube_C___API.Models;
-using Microsoft.EntityFrameworkCore;
-
-
-public class AppContext : DbContext
-{
-        public AppContext(DbContextOptions<AppContext> options) : base(options) {}
-        public DbSet<User> Users { get; set; } = null!;
-}
