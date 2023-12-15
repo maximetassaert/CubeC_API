999963edb2170cb0df54b287831250c62a96f5fc
Maxime Tassaert <m.tassaert@outlook.fr>
2023-12-15 10:39:50 +0100

    add products and cart to db

--- Cube C - API/AppContext.cs
+++ Cube C - API/AppContext.cs
@@ -7,6 +7,10 @@ public class AppContext : DbContext
         public AppContext(DbContextOptions<AppContext> options) : base(options) {}
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Cart> Carts { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
