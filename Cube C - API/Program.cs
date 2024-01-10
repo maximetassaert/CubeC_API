4670ac1f7594c33621294738d992b119e9e95232
Maxime Tassaert <m.tassaert@outlook.fr>
2024-01-10 16:38:58 +0100

    init react with login page

--- Cube C - API/Program.cs
+++ Cube C - API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using AppContext = Cube_C___API.AppContext;
 
 DotNetEnv.Env.Load();
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,6 +41,15 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*").AllowAnyHeader();;
+        });
+});
+
 builder.Services.AddMvc(config =>
 {
     var policy = new AuthorizationPolicyBuilder()
@@ -58,6 +68,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 app.UseAuthorization();
